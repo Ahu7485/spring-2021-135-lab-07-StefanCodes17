3e93e607af7c0b8a622b0adef8c18ad7b44572b0
StefanCodes17 <sk893q@gmail.com>
2021-03-22 07:47:28 -0400

    Didn't catch the part about adding a input.cpp so I quickly added it now, hopefully it isn't a problem!

---
+++ input.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "editor.h"
+
+int main()
+{
+    int blockCount = 0;
+    string userInputLine;
+    while (getline(cin, userInputLine))
+    {
+        string output = "";
+        if (removeLeadingSpaces(userInputLine)[0] == '}')
+        {
+            blockCount--;
+        }
+        for (int i = 0; i < blockCount; ++i)
+        {
+            output += '\t';
+        }
+        output += removeLeadingSpaces(userInputLine);
+        cout << output << endl;
+        blockCount += countChar(userInputLine, '{');
+        //Since we removed a blockCount already
+        //We want to add it back because it was just for that line
+        if (removeLeadingSpaces(userInputLine)[0] == '}')
+        {
+            blockCount++;
+        }
+        blockCount -= countChar(userInputLine, '}');
+    }
+}
